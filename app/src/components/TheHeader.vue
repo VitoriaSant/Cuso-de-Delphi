f4ac6352b7ee7b92922cc6e044a228e168d354d3
VitoriaSant <svitoria3465@gmail.com>
2024-05-13 22:39:20 -0300

    Estrutura inicial e alguns teste

---
+++ app/src/components/TheHeader.vue
@@ -0,0 +1,35 @@
+<template>
+
+
+<div 
+
+    v-for="obj in array
+    v-bind:key="obj.id" 
+"> 
+//Precisa colocar uma key para identificar o obj e a impressao ser mais rapido e com menos recursos
+//v-bind e para indicar que o "obj.id" e do array e náo uma string qualquer
+    {{ obj.name}}
+</div>
+
+    
+</template>
+<script>
+import Nome from 'caminho';
+
+export default{
+    name: 'App'
+    components: {
+        Nome
+    }
+},
+data(){
+    return{
+        Variaveis //Tudo declarado aqui fica disponivel para ser usado no componente 
+    }
+}
+
+
+</script>
+<style >
+
+</style>
